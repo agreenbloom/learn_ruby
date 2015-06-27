70c30c559da38819eaca38b3225a9e1b66f985fa
Abby Greenbloom <abby.greenbloom@gmail.com>
2015-06-27 17:02:15 -0400

    finished exercise 8

---
+++ 08_timer/timer.rb
@@ -0,0 +1,19 @@
+class Timer
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  # def seconds=(seconds)
+  #   @seconds = seconds
+
+  def time_string
+   Time.at(@seconds).utc.strftime  Time.at(@seconds).utc.strftime "%H:%M:%S"
+
+  end
+
+
+end
+
+
