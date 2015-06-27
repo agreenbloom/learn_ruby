c755aa7c6a647b3492b6a18a49e5b41e0388ee03
Abby Greenbloom <abby.greenbloom@gmail.com>
2015-06-27 14:27:31 -0400

    completed exercise 7

--- 07_book_titles/Book.rb
+++ 07_book_titles/Book.rb
@@ -1,12 +1,18 @@
-def title(string)
-  ignore = %w[the, a, an, and, in]
+class Book
+  attr_reader :title
 
-  word.map do |word|
-  if ignore.include? word
-    string.downcase
-  else
-    word.capitalize
+  def title=(new_title)
+    words = new_title.split(" ")
+    words = [words[0].capitalize] +
+      words[1..-1].map do |word|
+        ignore = %w{a an and the in of}
+        if ignore.include? word
+          word
+        else
+          word.capitalize
+        end
+      end
+    @title = words.join(" ")
   end
-end
-end
 
+end
